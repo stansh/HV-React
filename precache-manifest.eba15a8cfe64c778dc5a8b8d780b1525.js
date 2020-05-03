0ee60be9661f69d7b0c4d401fce6924b008b546c
Stan S <stan.shm@gmail.com>
2020-05-03 16:38:24 -0500

    Updates

---
+++ precache-manifest.eba15a8cfe64c778dc5a8b8d780b1525.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc11532cc6386a55b835c5dd21f7219",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28776af61fd76bd9b262",
+    "url": "/static/css/2.d8a29c9c.chunk.css"
+  },
+  {
+    "revision": "8564df4abb270813f139",
+    "url": "/static/css/main.978a3473.chunk.css"
+  },
+  {
+    "revision": "28776af61fd76bd9b262",
+    "url": "/static/js/2.f5cdcfb3.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.f5cdcfb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8564df4abb270813f139",
+    "url": "/static/js/main.52e75f76.chunk.js"
+  },
+  {
+    "revision": "ca15040073ac19e95fba",
+    "url": "/static/js/runtime-main.10a77f42.js"
+  },
+  {
+    "revision": "4f95a7d5181cecbd9d7ebf7aae5d7526",
+    "url": "/static/media/amatic-sc-latin-400.4f95a7d5.woff2"
+  },
+  {
+    "revision": "600b83a700c17addeb9cd23b8545d925",
+    "url": "/static/media/amatic-sc-latin-400.600b83a7.woff"
+  },
+  {
+    "revision": "65dcf7cc973b58effdc0ef94b785c0b9",
+    "url": "/static/media/amatic-sc-latin-700.65dcf7cc.woff"
+  },
+  {
+    "revision": "b49b921ad89d4948a8d073c523793ec0",
+    "url": "/static/media/amatic-sc-latin-700.b49b921a.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "18de8fc0e0e4cd033d232708334d331d",
+    "url": "/static/media/kalam-latin-300.18de8fc0.woff2"
+  },
+  {
+    "revision": "2d33bee3b96ac62e7c9f0613056190bd",
+    "url": "/static/media/kalam-latin-300.2d33bee3.woff"
+  },
+  {
+    "revision": "84d8bd8ef6effbbdcfed3a02173373a1",
+    "url": "/static/media/kalam-latin-400.84d8bd8e.woff2"
+  },
+  {
+    "revision": "c9ae72400c536d5cc7d00d1f51f07db1",
+    "url": "/static/media/kalam-latin-400.c9ae7240.woff"
+  },
+  {
+    "revision": "bf8f2fe89a820d12427d7aaf1ee9af4c",
+    "url": "/static/media/kalam-latin-700.bf8f2fe8.woff"
+  },
+  {
+    "revision": "dcaa2d3f62088d108ebfe66486c8be57",
+    "url": "/static/media/kalam-latin-700.dcaa2d3f.woff2"
+  }
+]);
